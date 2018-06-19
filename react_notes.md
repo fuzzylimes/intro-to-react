1bfb24a7a6f21f62233b744c97805408720c09ac
zcochran <fuzzylimes@gmail.com>
2018-06-19 05:44:52 -0400

    Referencing between two components

--- react_notes.md
+++ react_notes.md
@@ -28,3 +28,17 @@ Note that this is huge: ~134MB when building a new project.
 * Use `props` to access arguments passed to components
 * Use `props.children` to access values passed betwen component tabs (i.e. `<Person>This is a test</Person>`)
 * If the state property changes, it will cause the element to be re-rendered.
+
+## Handler Functions
+* Use the label `Handler` to note handler methods inside of components
+* DO NOT use `()` for method calls, just pass reference
+* Supported events: https://reactjs.org/docs/events.html#supported-events
+* If state or props changes, react will check to see if something would change on DOM, and update to reflect those changes.
+    * These are the only two things that can be used to cause an update on the page
+* State should only be updated in select components (containers). The rest of the smaller components should NOT change the state.
+
+## Method References between Components
+* You can pass in a method of one component to another using props
+* Can use `bind` to pass in a parameter to a method
+* Can also use a function call to do the update
+    * Can affect perfomance, bind is recommended.
--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ class App extends Component {
       { name: 'fuzzylimes', age: 30 },
       { name: 'Bob', age: 45 },
       { name: 'Jane', age: 39 }
-    ]
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // this.state.persons[0].name = 'Zach';
+    this.setState({
+      persons: [
+        { name: newName, age: 30 },
+        { name: 'Bob', age: 45 },
+        { name: 'Jane', age: 38 }
+      ]
+    })
   }
 
   render() {
@@ -16,10 +29,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Cooking</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Zach')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'fuzzylimes')}>Hobbies: Cooking</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     // All of the JSX above gets converted to the following when compiled.
