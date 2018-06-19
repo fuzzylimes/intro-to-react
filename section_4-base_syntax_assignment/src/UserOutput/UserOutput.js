1a5e5b8e72c52d15d1ce0e123456b5a7898c1063
zcochran <fuzzylimes@gmail.com>
2018-06-19 19:55:48 -0400

    Mislabled all of the directories...

---
+++ section_4-base_syntax_assignment/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useroutput = (props) => {
+    return (
+        <div>
+            <p>{props.p1}</p>
+            <p>{props.p2}</p>
+        </div>
+    )
+}
+
+export default useroutput;
